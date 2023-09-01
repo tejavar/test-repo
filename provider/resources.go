c1fcdf3b14892415affbdfb6a1cc2fcee198d96b
Anton Tayanovskyy <anton@pulumi.com>
2023-09-01 14:05:04 -0400

    Cleanup

--- provider/resources.go
+++ provider/resources.go
@@ -24,7 +24,7 @@ import (
 	shimv2 "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/sdk-v2"
 	"github.com/pulumi/pulumi-xyz/provider/pkg/version"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
-	"github.com/terraform-providers/terraform-provider-xyz/xyz"
+	xyz "github.com/terraform-providers/terraform-provider-panos/panos"
 )
 
 // all of the token components used below.
